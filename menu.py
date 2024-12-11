f71e6d41f7b97d4c32b6bdff00c8310c67a3e10c
Mayukh Mitra <156567859+CelestiaLKamI@users.noreply.github.com>
2024-12-11 21:29:10 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,7 @@
 print("Loading menu.py...")
 
+menu = nuke.menu("Nuke")
+my_scripts = menu.addMenu("Mayukh Scripts")
+
 from shot_builder import *
 my_scripts.addCommand("Shot Builder", shot_builder)
