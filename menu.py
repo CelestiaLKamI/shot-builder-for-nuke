5f99ec32c0b5ae3c74cf9b85c4640b1a9bc310e8
CelestiaLKamI <work.mayukh14@gmail.com>
2024-12-27 21:55:44 +0530

    menu.py update

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ from shot_builder import shot_builder
 print("Loading menu.py...")
 
 menu = nuke.menu("Nuke")
-my_scripts = menu.addMenu("Mayukh Scripts Testing")
+my_scripts = menu.addMenu("Mayukh Scripts")
 
 shot_builder_tool = my_scripts.addMenu("Shot Builder Tools")
 shot_builder_tool.addCommand("Set Production Path", set_prod_path)
